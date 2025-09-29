4f9fc5174de18a4ff449ab525ea85534b57bd649
Samruddhi Pathre <samruddhipathre16@gmail.com>
2025-09-29 22:08:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ✅ Add this line
+  base: "/Aavatar/",
+
   server: {
     host: "::",
     port: 8080,
